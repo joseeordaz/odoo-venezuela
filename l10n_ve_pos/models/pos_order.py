147788445d210e8cf2a74c99d9aff696d6941462
root <root@hola-odoo.home.arpa>
2026-08-03 18:04:54 -0400

    [FIX] l10n_ve_pos: adapt order hooks to Odoo 19

--- l10n_ve_pos/models/pos_order.py
+++ l10n_ve_pos/models/pos_order.py
@@ -12,11 +12,6 @@ class PosOrder(models.Model):
     foreign_amount_total = fields.Float(string="Foreign Total", readonly=True, required=True)
     foreign_currency_rate = fields.Float(readonly=True, required=False)
     
-    def _process_order(self, order, draft, existing_order):
-        res = super()._process_order(order, draft, existing_order)
-        order = self.browse(res)
-        return res
-
     @api.model
     def _order_fields(self, ui_order):
         res = super()._order_fields(ui_order)
@@ -52,8 +47,8 @@ class PosOrder(models.Model):
         return self.payment_ids.read()
 
     @api.model
-    def _get_invoice_lines_values(self, line_values, pos_order_line):
-        res = super()._get_invoice_lines_values(line_values, pos_order_line)
+    def _get_invoice_lines_values(self, line_values, pos_order_line, move_type):
+        res = super()._get_invoice_lines_values(line_values, pos_order_line, move_type)
         res["foreign_price"] = pos_order_line.foreign_price
         return res
 
