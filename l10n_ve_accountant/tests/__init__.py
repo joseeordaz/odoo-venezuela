5e410a6d217741a071afca71b8f6a19189ef7cb3
joseeordaz <95986093+joseeordaz@users.noreply.github.com>
2026-08-21 16:27:09 -0500

    test(accountant): isolate multicompany journal regression

--- l10n_ve_accountant/tests/__init__.py
+++ l10n_ve_accountant/tests/__init__.py
@@ -1,4 +1,5 @@
 from . import test_accountant
+from . import test_account_journal
 from . import test_account_payment_rate
 from . import test_foreign_balance
 from . import test_multi_currency_rounding
---
+++ l10n_ve_accountant/tests/test_account_journal.py
@@ -0,0 +1,36 @@
+from odoo.exceptions import ValidationError
+from odoo.tests import TransactionCase, tagged
+
+
+@tagged("post_install", "-at_install", "l10n_ve_accountant_journal")
+class TestAccountJournal(TransactionCase):
+    def test_international_purchase_journal_is_unique_per_company(self):
+        currency = self.env.company.currency_id
+        company_a, company_b = self.env["res.company"].create([
+            {"name": "International Journal Company A", "currency_id": currency.id},
+            {"name": "International Journal Company B", "currency_id": currency.id},
+        ])
+        Journal = self.env["account.journal"].sudo()
+        Journal.create({
+            "name": "International Purchases A",
+            "code": "INPA",
+            "type": "purchase",
+            "company_id": company_a.id,
+            "is_purchase_international": True,
+        })
+        with self.assertRaises(ValidationError):
+            Journal.create({
+                "name": "Duplicate International Purchases A",
+                "code": "DIPA",
+                "type": "purchase",
+                "company_id": company_a.id,
+                "is_purchase_international": True,
+            })
+        journal_b = Journal.create({
+            "name": "International Purchases B",
+            "code": "INPB",
+            "type": "purchase",
+            "company_id": company_b.id,
+            "is_purchase_international": True,
+        })
+        self.assertTrue(journal_b.is_purchase_international)
